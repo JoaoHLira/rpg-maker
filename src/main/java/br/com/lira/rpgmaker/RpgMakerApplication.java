ad519f8eab6264d3ceb16a7dd7cb41cef91bfad8
Your Name <you@example.com>
2023-04-05 09:32:58 -0300

    implementa API de teste

---
+++ src/main/java/br/com/lira/rpgmaker/RpgMakerApplication.java
@@ -0,0 +1,22 @@
+package br.com.lira.rpgmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class RpgMakerApplication {
+
+	@GetMapping
+	public String getHomeTest() {
+		return "Rpg Maker - API Home";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RpgMakerApplication.class, args);
+	}
+}
