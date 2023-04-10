82e651ee22d83f575faddf036d55b04ec88df9f2
Your Name <you@example.com>
2023-04-10 16:34:02 -0300

    implementa controller e fluxo service

--- src/main/java/br/com/lira/rpgmaker/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/lira/rpgmaker/cliente/application/api/ClienteController.java
@@ -2,17 +2,22 @@ package br.com.lira.rpgmaker.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.lira.rpgmaker.cliente.application.service.ClienteService;
 import br.com.lira.rpgmaker.cliente.domain.ClienteAPI;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+@RequiredArgsConstructor
 @RestController
 @Log4j2
 public class ClienteController implements ClienteAPI {
+	private final ClienteService clienteService;
 
 	@Override
 	public ClienteResponse postCliente(@Valid ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteController - postCliente");
+		ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
 		log.info("[finaliza] ClienteController - postCliente");
 		return null;
 	}
---
+++ src/main/java/br/com/lira/rpgmaker/cliente/application/service/ClienteService.java
@@ -0,0 +1,14 @@
+package br.com.lira.rpgmaker.cliente.application.service;
+
+import br.com.lira.rpgmaker.cliente.application.api.ClienteRequest;
+import br.com.lira.rpgmaker.cliente.application.api.ClienteResponse;
+import jakarta.validation.Valid;
+
+public class ClienteService {
+
+	public ClienteResponse criaCliente(@Valid ClienteRequest clienteRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
