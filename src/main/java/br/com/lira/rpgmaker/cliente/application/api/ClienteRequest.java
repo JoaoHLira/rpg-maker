7f51bee44ef47d25d63d68645483809174c8d526
Your Name <you@example.com>
2023-04-10 16:38:46 -0300

    implementa request e response

--- src/main/java/br/com/lira/rpgmaker/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/lira/rpgmaker/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,19 @@
 package br.com.lira.rpgmaker.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	@NotBlank
+	private String nome;
+	@NotBlank
+	@Email
+	private String email;
+	@NotNull
+	private LocalDate dataHoraCadastro;
 }
